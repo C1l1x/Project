be44c128368360d5a7ea13d5d8e787fc060feba9
a <a>
2023-09-22 11:43:42 +0300

    init

--- notes.md
+++ notes.md
@@ -5,11 +5,12 @@ Make a To-Do List Schedule
 Start with design (try to not look awful)
 
 Functionality
+- if there is no tasks (display no task)
 - add new task -- https://www.geeksforgeeks.org/html-dom-button-object/
 - create a toggle button for complete and no complete task -- https://habr.com/en/articles/490744/  --- use background-size and position (absolute)
 - change a button on click -- https://www.w3schools.com/howto/howto_js_toggle_class.asp
+- save current data -- https://www.tutorialspoint.com/How-can-I-save-HTML-locally-with-JavaScript (or ask Dzhem Idrizov)
 - delete/remove task -- https://www.geeksforgeeks.org/how-to-remove-an-html-element-using-javascript/
-- save current data -- https://www.tutorialspoint.com/How-can-I-save-HTML-locally-with-JavaScript (or ask Djem Idrizov)
 
 
 
--- script.js
+++ script.js
@@ -1,14 +1,39 @@
 const userInput = document.getElementById("input");
 const listTask = document.getElementById("listTask");
 
+// function to add new task to list
 function addTask() {
     let task = document.createElement("li");
     task.innerHTML = userInput.value;
     listTask.appendChild(task);
     userInput.value = "";
+    let span = document.createElement("span");
+    span.innerHTML = "x";
+    task.appendChild(span);
+
+    saveData()
 }
 
-listTask.addEventListener("click", function() {
-    const li = document.getElementById("li");
-    li.classList.toggle("done");
-});
+// function to toggle a list as "done"
+listTask.addEventListener("click", function(Done) {
+    if(Done.target.tagName === "LI"){
+        Done.target.classList.toggle("done");
+        saveData()
+    }
+    else if(Done.target.tagName === "SPAN") {
+        Done.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+// function to saving data in the browser
+function saveData() {
+    localStorage.setItem("data", listTask.innerHTML)
+}
+
+// function to call saved data when refresh the page
+function showSavedData() {
+    listTask.innerHTML = localStorage.getItem("data");
+}
+
+showSavedData();
